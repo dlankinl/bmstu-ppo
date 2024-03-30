db746ce62114bd10451b292233b17db28c7f73ad
dmitry <dmit.lankin@mail.ru>
2024-03-30 23:21:18 +0300

    removed useless code

--- backend/services/fin_report/fin_report.go
+++ backend/services/fin_report/fin_report.go
@@ -36,7 +36,7 @@ func (s *Service) Create(finReport *domain.FinancialReport) (err error) {
 		return fmt.Errorf("значение года не может быть больше текущего года")
 	}
 
-	if finReport.Year == now.Year() && finReport.Quarter > (int(now.Month()-1)/3) { // TODO: проверить юнит-тестами
+	if finReport.Year == now.Year() && finReport.Quarter > (int(now.Month()-1)/3) {
 		return fmt.Errorf("нельзя добавить отчет за квартал, который еще не закончился")
 	}
 
@@ -85,60 +85,6 @@ func (s *Service) GetByCompany(companyId uuid.UUID, period *domain.Period) (
 	if err != nil {
 		return nil, fmt.Errorf("получение финансового отчета по id компании: %w", err)
 	}
-
-	//yearReports := make(map[int]domain.FinancialReportByPeriod)
-	//
-	//var i int
-	//for year := period.StartYear; year <= period.EndYear; year++ {
-	//	startQtr := 1
-	//	endQtr := 4
-	//
-	//	if year == period.StartYear {
-	//		startQtr = period.StartQuarter
-	//	}
-	//	if year == period.EndYear {
-	//		endQtr = period.EndQuarter
-	//	}
-	//
-	//	var totalFinReport domain.FinancialReportByPeriod
-	//	for quarter := startQtr; quarter <= endQtr; quarter++ {
-	//		totalFinReport.Reports = append(totalFinReport.Reports, finReport.Reports[i])
-	//		i++
-	//	}
-	//
-	//	per := &domain.Period{
-	//		StartYear:    year,
-	//		EndYear:      year,
-	//		StartQuarter: startQtr,
-	//		EndQuarter:   endQtr,
-	//	}
-	//	totalFinReport.Period = per
-	//	yearReports[year] = totalFinReport
-	//}
-	//
-	//for _, v := range yearReports {
-	//	if len(v.Reports) == 4 {
-	//		totalProfit := v.Profit()
-	//		var taxFare int
-	//		switch true {
-	//		case totalProfit < 10000000:
-	//			taxFare = 4
-	//		case totalProfit < 50000000:
-	//			taxFare = 7
-	//		case totalProfit < 150000000:
-	//			taxFare = 13
-	//		case totalProfit < 500000000:
-	//			taxFare = 20
-	//		default:
-	//			taxFare = 30
-	//		}
-	//
-	//		v.Taxes = totalProfit * (float32(taxFare) / 100)
-	//
-	//		finReport.Taxes += v.Taxes
-	//		finReport.TaxLoad += v.Taxes / v.Revenue() * 100
-	//	}
-	//}
 
 	return finReport, nil
 }
