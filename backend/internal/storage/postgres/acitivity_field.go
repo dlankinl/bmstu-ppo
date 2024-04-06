f9e5a0a1d38c4e2cdbab6a8d2ce081302cba547c
dmitry <dmit.lankin@mail.ru>
2024-04-07 00:36:54 +0300

    repos update

--- backend/internal/storage/postgres/acitivity_field.go
+++ backend/internal/storage/postgres/acitivity_field.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx"
 	"ppo/domain"
@@ -11,26 +12,131 @@ type ActivityFieldRepository struct {
 	db *pgx.Conn
 }
 
-func (r *ActivityFieldRepository) Create(context.Context, *domain.ActivityField) (err error) {
+func (r *ActivityFieldRepository) Create(ctx context.Context, data *domain.ActivityField) (err error) {
+	query := `insert into ppo.activity_fields(name, description, cost) 
+	values ($1, $2, $3)`
+
+	_, err = r.db.ExecEx(
+		ctx,
+		query,
+		nil,
+		data.Name,
+		data.Description,
+		data.Cost,
+	)
+	if err != nil {
+		return fmt.Errorf("создание сферы деятельности: %w", err)
+	}
+
 	return nil
 }
 
-func (r *ActivityFieldRepository) DeleteById(context.Context, uuid.UUID) (err error) {
+func (r *ActivityFieldRepository) DeleteById(ctx context.Context, id uuid.UUID) (err error) {
+	query := `delete from ppo.activity_fields where id = $1`
+
+	_, err = r.db.ExecEx(
+		ctx,
+		query,
+		nil,
+		id,
+	)
+	if err != nil {
+		return fmt.Errorf("удаление сферы деятельности по id: %w", err)
+	}
+
 	return nil
 }
 
-func (r *ActivityFieldRepository) Update(context.Context, *domain.ActivityField) (err error) {
+func (r *ActivityFieldRepository) Update(ctx context.Context, data *domain.ActivityField) (err error) {
+	query := `
+			update ppo.activity_fields
+			set 
+			    name = $1,
+			    description = $2, 
+			    cost = $3
+			where id = $4`
+
+	_, err = r.db.ExecEx(
+		ctx,
+		query,
+		nil,
+		data.Name,
+		data.Description,
+		data.Cost,
+		data.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("обновление информации о сфере деятельности: %w", err)
+	}
+
 	return nil
 }
 
-func (r *ActivityFieldRepository) GetById(context.Context, uuid.UUID) (field *domain.ActivityField, err error) {
+func (r *ActivityFieldRepository) GetById(ctx context.Context, id uuid.UUID) (field *domain.ActivityField, err error) {
+	query := `select name, description, cost from ppo.activity_fields where id = $1`
+
+	data = new(domain.ActivityField)
+	err = r.db.QueryRowEx(
+		ctx,
+		query,
+		nil,
+		id,
+	).Scan(
+		&data.Name,
+		&data.Description,
+		&data.Cost,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("получение сферы деятельности по id: %w", err)
+	}
+
 	return nil, nil
 }
 
-func (r *ActivityFieldRepository) GetByCompanyId(context.Context, uuid.UUID) (cost float32, err error) {
+// TODO: FIX
+func (r *ActivityFieldRepository) GetByCompanyId(ctx context.Context, id uuid.UUID) (cost float32, err error) {
+	// query := `select id, name, description, cost from ppo.activity_fields where owner_id = $1 `
+
+	// rows, err := r.db.QueryEx(
+	// 	ctx,
+	// 	query,
+	// 	nil,
+	// 	id,
+	// )
+
+	// contacts = make([]*domain.Contact, 0)
+	// for rows.Next() {
+	// 	tmp := new(domain.Contact)
+
+	// 	err = rows.Scan(
+	// 		&tmp.ID,
+	// 		&tmp.Name,
+	// 		&tmp.Value,
+	// 	)
+	// 	tmp.OwnerID = id
+
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("сканирование полученных строк: %w", err)
+	// 	}
+	// }
+
 	return 0, nil
 }
 
-func (r *ActivityFieldRepository) GetMaxCost(context.Context) (cost float32, err error) {
+func (r *ActivityFieldRepository) GetMaxCost(ctx context.Context) (cost float32, err error) {
+	query := `select max(cost)
+		from ppo.activity_fields`
+
+	var maxVal float32
+	err = r.db.QueryRowEx(
+		ctx,
+		query,
+		nil,
+	).Scan(&maxVal)
+
+	if err != nil {
+		return 0, fmt.Errorf("получение максимального веса сферы деятельности: %w", err)
+	}
+
 	return 0, nil
 }
---
+++ backend/internal/storage/postgres/user_skill.go
@@ -0,0 +1,116 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx"
+	"ppo/domain"
+)
+
+type UserSkillRepository struct {
+	db *pgx.Conn
+}
+
+func NewUserSkillRepository(db *pgx.Conn) domain.IUserSkillRepository {
+	return &UserSkillRepository{
+		db: db,
+	}
+}
+
+func (r *UserSkillRepository) Create(ctx context.Context, pair *domain.UserSkill) (err error) {
+	query := `insert into ppo.user_skills(user_id, skill_id) 
+	values ($1, $2)`
+
+	_, err = r.db.ExecEx(
+		ctx,
+		query,
+		nil,
+		pair.UserId,
+		pair.SkillId,
+	)
+	if err != nil {
+		return fmt.Errorf("создание cвязи пользователь-навык: %w", err)
+	}
+
+	return nil
+}
+
+func (r *UserSkillRepository) Delete(ctx context.Context, pair *domain.UserSkill) (err error) {
+	query := `delete from ppo.user_skills where user_id = $1 and skill_id = $2`
+
+	_, err = r.db.ExecEx(
+		ctx,
+		query,
+		nil,
+		pair.UserId,
+		pair.SkillId,
+	)
+	if err != nil {
+		return fmt.Errorf("удаление пары пользователь-навык: %w", err)
+	}
+
+	return nil
+}
+
+func (r *UserSkillRepository) GetUserSkillsByUserId(ctx context.Context, userId uuid.UUID) (pairs []*domain.UserSkill, err error) {
+	query := `select skill_id from ppo.user_skills where user_id = $1`
+
+	rows, err := r.db.QueryEx(
+		ctx,
+		query,
+		nil,
+		userId,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("получение навыков пользователя: %w", err)
+	}
+
+	pairs = make([]*domain.UserSkill, 0)
+	for rows.Next() {
+		tmp := new(domain.UserSkill)
+
+		err = rows.Scan(
+			&tmp.SkillId,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("сканирование строки: %w", err)
+		}
+
+		tmp.UserId = userId
+		pairs = append(pairs, tmp)
+	}
+
+	return pairs, nil
+}
+
+func (r *UserSkillRepository) GetUserSkillsBySkillId(ctx context.Context, skillId uuid.UUID) (pairs []*domain.UserSkill, err error) {
+	query := `select user_id from ppo.user_skills where skill_id = $1`
+
+	rows, err := r.db.QueryEx(
+		ctx,
+		query,
+		nil,
+		skillId,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("получение пользователей по навыку: %w", err)
+	}
+
+	pairs = make([]*domain.UserSkill, 0)
+	for rows.Next() {
+		tmp := new(domain.UserSkill)
+
+		err = rows.Scan(
+			&tmp.UserId,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("сканирование строки: %w", err)
+		}
+
+		tmp.SkillId = skillId
+		pairs = append(pairs, tmp)
+	}
+
+	return pairs, nil
+}
